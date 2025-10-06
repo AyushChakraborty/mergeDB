2ebe0226d424d0914814822a61a4eba125465779
Anirudh Sripada Koundinya <anirudhsripada05@gmail.com>
2025-10-06 14:09:40 +0530

    feat: implement the node config

--- kv-node/src/main.rs
+++ kv-node/src/main.rs
@@ -1,3 +1,15 @@
+use std::{path::Path};
+use kv_node::config::Config;
+
 fn main() {
-    println!("Hello, world!");
+    let path = "config.toml".to_string();
+    let file_path = Path::new(&path);
+    match Config::load_config(file_path.to_path_buf()) {
+        Ok(c) => {
+        println!("{:?}", c);
+        },
+        Err(e) => {
+            eprintln!("Error: {e}");
+        }
+    }
 }
