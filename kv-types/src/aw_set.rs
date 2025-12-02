d5d248e0820bbd395fdf320749d6f98aa9151ca6
AyushChakraborty <chakrabortyayush83@gmail.com>
2025-12-02 23:36:22 +0530

    feat(kv-types): implement merge method for AWSet

--- kv-types/src/aw_set.rs
+++ kv-types/src/aw_set.rs
@@ -1,5 +1,8 @@
 use super::Merge;
-use std::{collections::{HashMap, HashSet}, hash::Hash};
+use std::{
+    collections::{HashMap, HashSet},
+    hash::Hash,
+};
 
 pub struct AWSet<T>
 where
@@ -13,9 +16,8 @@ where
     //where the order need not be maintained as thats not the primary task at hand.
     //The primary task here is to ensure that additions and removals of tags are eventually
     //consistent across both the nodes
-
-    pub current_tags: HashSet<T>,                  //this is what reflects the actual set of tags
-    add_tags: HashMap<T, (u32, Vec<u32>)>,         //to not make finding the length O(n) time                 
+    pub current_tags: HashSet<T>, //this is what reflects the actual set of tags
+    add_tags: HashMap<T, (u32, Vec<u32>)>, //to not make finding the length O(n) time
     remove_tags: HashMap<T, (u32, Vec<u32>)>,
 }
 
@@ -23,36 +25,49 @@ impl<T> AWSet<T>
 where
     T: Eq + Hash + Clone,
 {
-    //PRINCIPLE FOLLOWED: never remove from add_tags or the remove_tags map, they are meant 
+    //PRINCIPLE FOLLOWED: never remove from add_tags or the remove_tags map, they are meant
     //to be totally self contained, and have the history of all the operations
 
-    //user can only deal with the current_tags set, the add_tags and remove_tags are 
+    //user can only deal with the current_tags set, the add_tags and remove_tags are
     //private to the definition of the type
 
     fn new(current_tags: HashSet<T>) -> Self {
-        AWSet { 
-            current_tags: current_tags, 
-            add_tags: HashMap::new(), 
+        AWSet {
+            current_tags: current_tags,
+            add_tags: HashMap::new(),
             remove_tags: HashMap::new(),
         }
     }
 
     fn add_tag(&mut self, tag: T) {
         //since its a set eventually, if the tag is not already in remove_tags map,
-        //and its not already in add_tags map, only then add it, in all other cases 
+        //and its not already in add_tags map, only then add it, in all other cases
 
-        let version_num = self.add_tags.get(&tag).expect("could not extract value").0;             //this way, the version number for the same tag 
-        //remains consistent across nodes, so this helps during merge()
+        let mut version_num = 0;
+        if self.add_tags.contains_key(&tag) {
+            version_num = self.add_tags.get(&tag).expect("could not extract value").0;
+            //this way, the version number for the same tag
+            //remains consistent across nodes, so this helps during merge()
+        }
 
-        //if tag does not already exist in both of the maps OR 
-        if !self.add_tags.contains_key(&tag) || !self.remove_tags.contains_key(&tag)  {
+        //if tag does not already exist in both of the maps OR
+        if !self.add_tags.contains_key(&tag) || !self.remove_tags.contains_key(&tag) {
             self.add_tags.insert(tag, (1, vec![version_num]));
         }
         //if lens of both the vectors for that tag are the same, it means that that tag no longer exists in the set
         //(equal number of additions and removals), so can add
-        else if self.add_tags.get(&tag).expect("could not extract value").0 == self.remove_tags.get(&tag).expect("could not extract value").0 {
-            let val = self.add_tags.get_mut(&tag).expect("could not extract value");
-            val.0 += 1;      //incrementing the len by 1
+        else if self.add_tags.get(&tag).expect("could not extract value").0
+            == self
+                .remove_tags
+                .get(&tag)
+                .expect("could not extract value")
+                .0
+        {
+            let val = self
+                .add_tags
+                .get_mut(&tag)
+                .expect("could not extract value");
+            val.0 += 1; //incrementing the len by 1
             val.1.push(version_num);
         }
     }
@@ -63,20 +78,28 @@ where
             return;
         }
 
-        let version_index_to_be_deleted: usize = (self.add_tags.get(&tag).expect("could not extract value").0 - 1) as usize;
-        let version_to_be_deleted = self.add_tags.get(&tag).expect("could not extract value").1[version_index_to_be_deleted];
+        let version_index_to_be_deleted: usize =
+            (self.add_tags.get(&tag).expect("could not extract value").0 - 1) as usize;
+        let version_to_be_deleted = self.add_tags.get(&tag).expect("could not extract value").1
+            [version_index_to_be_deleted];
 
         if !self.remove_tags.contains_key(&tag) {
-            let val = self.remove_tags.get_mut(&tag).expect("could not extract value");
+            let val = self
+                .remove_tags
+                .get_mut(&tag)
+                .expect("could not extract value");
             val.0 = 1;
             val.1 = vec![version_to_be_deleted];
-        }else {
-            let val = self.remove_tags.get_mut(&tag).expect("could not extract value");
+        } else {
+            let val = self
+                .remove_tags
+                .get_mut(&tag)
+                .expect("could not extract value");
             val.0 += 1;
             val.1.push(version_to_be_deleted);
         }
     }
-    
+
     //this method emulates the commit action within a node
     //SHLD BE CALLED AFTER IT HAS MERGED ITSELF WITH ALL OTHER NODES
     //POSSIBLE DISCREPANCY, SINCE WHEN IT WILL BE MERGED IS "EVENTUAL"
@@ -108,7 +131,6 @@ where
     }
 }
 
-
 impl<T> Merge for AWSet<T>
 where
     T: Eq + Hash + Clone,
@@ -117,8 +139,37 @@ where
         //update wrt each other
 
         //node1 merge, so for some tag t1, go through all of its version numbers in remove_tags map, and if a version
-        //number is in the other node's add_tags map for that tag t1 and not in the other node's remove_tags map, 
+        //number is in the other node's add_tags map for that tag t1 and not in the other node's remove_tags map,
         //then retain it, hence the name AddWins Set
-        ()     
+
+        //point here is to set self.current_tags
+
+        //call local_merge method first to get the initial current_tags
+        self.local_merge();
+
+        for tag in self.remove_tags.keys() {
+            let self_removed_values = &self.remove_tags.get(tag).expect("could not get value").1;
+            //let self_add_values = &self.add_tags.get(tag).expect("could not get value").1;
+            let other_remove_values = &other.remove_tags.get(tag).expect("could not get value").1;
+            let other_add_values = &other.add_tags.get(tag).expect("could not get value").1;
+            for v_no in self_removed_values {
+                if other_add_values.contains(&v_no) && !other_remove_values.contains(&v_no) {
+                    self.current_tags.insert(tag.clone());
+                }
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_add_tag() {
+        let mut replica_a: AWSet<String> = AWSet::new(HashSet::new());
+        let tag = String::from("apple");
+        replica_a.add_tag(tag.clone());
+        assert_eq!(replica_a.add_tags.contains_key(&tag), true);
     }
 }
